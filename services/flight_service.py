9541654969cdb27ccd53b711e28532b2b36daa4c
ttniishh <tanishpanchal2311@gmail.com>
2025-08-02 00:35:47 +0530

    Update flight_service.py

--- services/flight_service.py
+++ services/flight_service.py
@@ -2,9 +2,9 @@ from log_manager import log
 
 class FlightService:
     @staticmethod
-    def book_flight(booking_id: str, flight_type: str):
-        log(f"[{booking_id}] Flight booked: {flight_type}")
+    def book_flight(booking_id: str):
+        log(f"[{booking_id}] Flight booked")
 
     @staticmethod
     def rollback(booking_id: str):
-        log(f"[{booking_id}] Flight booking rolled back")
+        log(f"[{booking_id}] Flight booking canceled (rollback)")
